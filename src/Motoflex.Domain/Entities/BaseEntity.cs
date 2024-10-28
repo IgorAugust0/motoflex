cc717ca509cb8e6b6231495ac96f01fdb3877a72
IgorAugust0 <augustoigor269@gmail.com>
2024-10-28 18:43:01 -0300

    feat(domain): add base entity and repository interfaces

---
+++ src/Motoflex.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Motoflex.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}

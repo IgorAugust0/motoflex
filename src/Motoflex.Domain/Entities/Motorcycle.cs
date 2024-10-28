c07875d968f388b4bfae0913ec066f52b54666e2
IgorAugust0 <augustoigor269@gmail.com>
2024-10-28 19:02:16 -0300

    feat(domain): implement moto entity and repository interface

---
+++ src/Motoflex.Domain/Entities/Motorcycle.cs
@@ -0,0 +1,10 @@
+﻿namespace Motoflex.Domain.Entities
+{
+    public class Motorcycle(int year, string model, string licensePlate) : BaseEntity()
+    {
+        public int Year { get; set; } = year;
+        public string Model { get; set; } = model;
+        public string LicensePlate { get; set; } = licensePlate;
+        public bool Available { get; set; } = true;
+    }
+}
---
+++ src/Motoflex.Domain/Interfaces/Repositories/IMotoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Motoflex.Domain.Entities;
+
+namespace Motoflex.Domain.Interfaces.Repositories
+{
+    public interface IMotoRepository : IBaseRepository<Motorcycle>
+    {
+        IQueryable<Motorcycle> GetByLicensePlate(string licensePlate);
+        IQueryable<Motorcycle> GetRentals();
+    }
+}

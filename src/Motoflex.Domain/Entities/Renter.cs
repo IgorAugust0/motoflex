e366b0c606ce7a46cc855b577e7a1296129cc119
IgorAugust0 <augustoigor269@gmail.com>
2024-10-28 19:35:57 -0300

    feat(domain): implement renter entity and repository interface

---
+++ src/Motoflex.Domain/Entities/Renter.cs
@@ -0,0 +1,19 @@
+﻿namespace Motoflex.Domain.Entities
+{
+    public class Renter(string name, string cnpj, DateTime birthDate, string cnh, CnhType cnhType) : BaseEntity()
+    {
+        public string Name { get; set; } = name;
+        public string Cnpj { get; set; } = cnpj;
+        public DateTime Birthdate { get; set; } = birthDate;
+        public string Cnh { get; set; } = cnh;
+        public CnhType CnhType { get; set; } = cnhType;
+        public string? CnhImage { get; set; }
+    }
+
+    public enum CnhType
+    {
+        A,
+        B,
+        AB
+    }
+}
---
+++ src/Motoflex.Domain/Interfaces/Repositories/IRenterRepository.cs
@@ -0,0 +1,10 @@
+﻿using Motoflex.Domain.Entities;
+
+namespace Motoflex.Domain.Interfaces.Repositories
+{
+    public interface IRenterRepository : IBaseRepository<Renter>
+    {
+        IQueryable<Renter> GetRentals();
+        IQueryable<Renter> AvailableRentersForOrder();
+    }
+}

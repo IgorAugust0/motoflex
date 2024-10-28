46b269ef73e8b23172471bae2ac61e40f1da0a97
IgorAugust0 <augustoigor269@gmail.com>
2024-10-28 20:26:48 -0300

    feat(domain): implement order entity and repository interface

---
+++ src/Motoflex.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace Motoflex.Domain.Entities
+{
+    public class Order(decimal deliveryFee) : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public decimal DeliveryFee { get; set; } = deliveryFee;
+        public Status Status { get; set; } = Status.Available;
+        public Guid? CourierId { get; set; }
+    }
+
+    public enum Status
+    {
+        Available,
+        Accepted,
+        Delivered,
+    }
+}
---
+++ src/Motoflex.Domain/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Motoflex.Domain.Entities;
+
+namespace Motoflex.Domain.Interfaces.Repositories
+{
+    public interface IOrderRepository : IBaseRepository<Order>
+    {
+        IQueryable<Order> GetNotifiedOrders(Guid id);
+    }
+}

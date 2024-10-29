0ef17c329d9677c6293f68c13d0de79d654589d6
IgorAugust0 <augustoigor269@gmail.com>
2024-10-29 15:41:27 -0300

    feat(infra): add motorcycle repository implementation

---
+++ src/Motoflex.Infrastructure/Repositories/MotorcycleRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Motoflex.Domain.Entities;
+using Motoflex.Domain.Interfaces.Repositories;
+using Motoflex.Infrastructure.Contexts;
+
+namespace Motoflex.Infrastructure.Repositories
+{
+    public class MotorcycleRepository(AppDbContext context) : BaseRepository<Motorcycle>(context), IMotoRepository
+    {
+        public IQueryable<Motorcycle> GetByLicensePlate(string licensePlate)
+        {
+            return _context.Set<Motorcycle>()
+                .Where(m => EF.Functions.Like(m.LicensePlate, $"{licensePlate}%"));
+        }
+
+        public IQueryable<Motorcycle> GetRentals()
+        {
+            return _context.Set<Motorcycle>()
+                .Include(m => m.Rentals); // nameof(Motorcycle.Rentals)
+        }
+    }
+}

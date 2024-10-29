be9e58de8a53b1d11827a392cf4053ee86c9dc8b
IgorAugust0 <augustoigor269@gmail.com>
2024-10-29 10:37:56 -0300

    feat(infra): add admin context config

---
+++ src/Motoflex.Infrastructure/Contexts/AdminConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Motoflex.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Motoflex.Infrastructure.Contexts
+{
+    public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+    {
+        public void Configure(EntityTypeBuilder<Admin> builder)
+        {
+            builder.Property<Guid>("Id").IsRequired();
+            builder.HasKey("Id");
+        }
+    }
+}

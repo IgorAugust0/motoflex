ad1ba5433a6ad5d49efd077858c559f1d96b2dfc
IgorAugust0 <augustoigor269@gmail.com>
2024-10-29 11:00:42 -0300

    feat(infra): add motorcycle context config

---
+++ src/Motoflex.Infrastructure/Contexts/MotorcycleConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Motoflex.Domain.Entities;
+
+namespace Motoflex.Infrastructure.Contexts
+{
+    public class MotorcycleConfiguration : IEntityTypeConfiguration<Motorcycle>
+    {
+        public void Configure(EntityTypeBuilder<Motorcycle> builder)
+        {
+            builder.Property<Guid>("Id").IsRequired();
+
+            builder.Property("Year").IsRequired();
+
+            builder
+                .Property("Model")
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+
+            builder
+                .Property("LicensePlate")
+                .IsRequired()
+                .HasColumnType("char(7)");
+
+            builder.HasKey("Id");
+            builder.HasIndex("LicensePlate").IsUnique();
+        }
+    }
+}

d7208a264b6291ea9f1b4cc8b88474a6396fda24
IgorAugust0 <augustoigor269@gmail.com>
2024-10-30 18:13:47 -0300

    feat(application): add renter DTOs and mapper

---
+++ src/Motoflex.Application/DTOs/Requests/CreateRenter.cs
@@ -0,0 +1,26 @@
+﻿using Motoflex.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Motoflex.Application.DTOs.Requests
+{
+    public class CreateRenter
+    {
+        [Required(ErrorMessage = "O nome do usuário é obrigatório")]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = "O CNPJ é obrigatório")]
+        [RegularExpression(@"^\d{14}$", ErrorMessage = "CNPJ deve ter 14 digitos")]
+        public required string Cnpj { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória")]
+        public required DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "A CNH é obrigatória")]
+        [RegularExpression(@"^\d{12}$", ErrorMessage = "CNH deve ter 12 digitos")]
+        public required string Cnh { get; set; }
+
+        [Required(ErrorMessage = "O tipo de CNH é obrigatório")]
+        [RegularExpression(@"^[a-eA-E]{1,5}$", ErrorMessage = "Tipo de CNH pode ter no máximo 5 caracteres de 'A' a 'E'")]
+        public required CnhType CnhType { get; set; }
+    }
+}
---
+++ src/Motoflex.Application/Mappers/RenterMapper.cs
@@ -0,0 +1,19 @@
+﻿using Motoflex.Application.DTOs.Requests;
+using Motoflex.Domain.Entities;
+
+namespace Motoflex.Application.Mappers
+{
+    public static class RenterMapper
+    {
+        public static Renter Map(this CreateRenter request)
+        {
+            return new Renter(
+                request.Name,
+                request.Cnpj,
+                request.BirthDate,
+                request.Cnh,
+                request.CnhType
+                );
+        }
+    }
+}

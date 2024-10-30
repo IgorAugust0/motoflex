96790dcdca2460cddc5ee8a255662e476432411d
IgorAugust0 <augustoigor269@gmail.com>
2024-10-30 19:40:21 -0300

    feat(application): add authentication token response DTO

---
+++ src/Motoflex.Application/DTOs/Responses/AuthenticationToken.cs
@@ -0,0 +1,7 @@
+﻿namespace Motoflex.Application.DTOs.Responses
+{
+    public class AuthenticationToken(string token)
+    {
+        public string Token { get; init; } = token; // immutable after initialization
+    }
+}

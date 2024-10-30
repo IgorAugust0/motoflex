4d5b961c5d065ea889eda0495a49e03d53a0cf2f
IgorAugust0 <augustoigor269@gmail.com>
2024-10-30 19:10:07 -0300

    feat(application): add order DTO and mapper

---
+++ src/Motoflex.Application/DTOs/Requests/CreateOrder.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Motoflex.Application.DTOs.Requests
+{
+    public class CreateOrder
+    {
+        [Required(ErrorMessage = "Valor da corrida é obrigatório")]
+        public required decimal DeliveryFee { get; set; }
+    }
+}
---
+++ src/Motoflex.Application/Mappers/OrderMapper.cs
@@ -0,0 +1,13 @@
+﻿using Motoflex.Application.DTOs.Requests;
+using Motoflex.Domain.Entities;
+
+namespace Motoflex.Application.Mappers
+{
+    public static class OrderMapper
+    {
+        public static Order Map(this CreateOrder request)
+        {
+            return new Order(request.DeliveryFee);
+        }
+    }
+}

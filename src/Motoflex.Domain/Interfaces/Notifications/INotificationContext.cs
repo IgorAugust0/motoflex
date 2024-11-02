7fe887a79817e7a9fcc1e667832fccf9ba8a4e61
IgorAugust0 <augustoigor269@gmail.com>
2024-11-02 13:22:02 -0300

    refactor: improve error handling - notif. context

---
+++ src/Motoflex.Domain/Interfaces/Notifications/INotificationContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Motoflex.Domain.Interfaces.Notifications
+{
+    public interface INotificationContext
+    {
+        bool HasNotifications { get; }
+        List<string> Notifications { get; }
+        void AddNotification(string message);
+        void AddNotifications(List<string> messages);
+    }
+}
---
+++ src/Motoflex.Infrastructure/Contexts/NotificationContext.cs
@@ -0,0 +1,25 @@
+﻿using Motoflex.Domain.Interfaces.Notifications;
+
+namespace Motoflex.Infrastructure.Contexts
+{
+    public class NotificationContext : INotificationContext
+    {
+        private readonly List<string> _notifications;
+        public List<string> Notifications { get { return _notifications; } }
+        public bool HasNotifications => _notifications.Count != 0; // Any()
+
+        public NotificationContext()
+        {
+            _notifications = [];
+        }
+
+        public void AddNotification(string message)
+        {
+            _notifications.Add(message);
+        }
+        public void AddNotifications(List<string> messages)
+        {
+            _notifications.AddRange(messages);
+        }
+    }
+}

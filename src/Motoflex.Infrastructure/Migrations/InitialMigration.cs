2e893625d9585ebc6bea06aff4d0d243c5a603f1
IgorAugust0 <augustoigor269@gmail.com>
2024-11-04 00:35:21 -0300

    feat(infra): add initial db migrations prep
    * this migration setup is not as good as it could be and will be improved later, also considering switching to fluent migration

---
+++ src/Motoflex.Infrastructure/Migrations/InitialMigration.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Motoflex.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Renter",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar", nullable: false),
+                    Cnpj = table.Column<string>(type: "char(14)", nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Cnh = table.Column<string>(type: "char(12)", nullable: false),
+                    CnhType = table.Column<string>(type: "varchar", nullable: false),
+                    CnhImage = table.Column<string>(type: "varchar", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Renter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Motorcycle",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Model = table.Column<string>(type: "varchar", maxLength: 50, nullable: false),
+                    LicensePlate = table.Column<string>(type: "char(7)", nullable: false),
+                    Available = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Motorcycle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DeliveryFee = table.Column<decimal>(type: "numeric(5,2)", precision: 5, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "varchar", nullable: false),
+                    RenterId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Renter_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Renter",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rental",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Plan = table.Column<string>(type: "varchar", nullable: false),
+                    RenterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MotorcycleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BeginAt = table.Column<DateTime>(type: "Date", nullable: false),
+                    FinishAt = table.Column<DateTime>(type: "Date", nullable: false),
+                    ReturnAt = table.Column<DateTime>(type: "Date", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rental", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rental_Renter_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Renter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rental_Motorcycle_MotorcycleId",
+                        column: x => x.MotorcycleId,
+                        principalTable: "Motorcycle",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RenterId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => new { x.OrderId, x.RenterId });
+                    table.ForeignKey(
+                        name: "FK_Notifications_Renter_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "Renter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Renter_Cnh",
+                table: "Renter",
+                column: "Cnh",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Renter_Cnpj",
+                table: "Renter",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Motorcycle_LicensePlate",
+                table: "Motorcycle",
+                column: "LicensePlate",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_RenterId",
+                table: "Notifications",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_RenterId",
+                table: "Order",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_RenterId",
+                table: "Rental",
+                column: "RenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_MotorcycleId",
+                table: "Rental",
+                column: "MotorcycleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Rental");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Motorcycle");
+
+            migrationBuilder.DropTable(
+                name: "Renter");
+        }
+    }
+}
